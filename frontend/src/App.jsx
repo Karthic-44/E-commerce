51519e2bd31c0db2e805cffc067347f7cc806f45
Karthic <karthics004@gmail.com>
2025-09-30 16:46:26 +0530

    Added ProductCard and Products

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
+import Products from './components/Products'
 
 function App() {
   return (
-   <h1 class="flex items-center justify-center h-screen bg-gray-800
-              text-white text-2xl font-bold">
-    Hello world!
-  </h1>
+    <>
+      <Products />
+    </>
   )
 }
 
