7879cc1b6a1cec57d922930cbfc4717f63389adc
Karthic <karthics004@gmail.com>
2025-10-26 13:32:09 +0530

    Added Loader

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -5,6 +5,7 @@ import { useEffect } from "react";
 import { fetchCategories, fetchProducts } from "../store/action";
 import Filter from "./Filter";
 import useProductFilter from "./useProductFilter";
+import Loader from "./Loader";
 
 
 const Products = () => {
@@ -28,7 +29,7 @@ const Products = () => {
         <div className="lg:px-14 sm:px-8 px-4 py-14 2xl:w-[90%] 2xl:mx-auto">
             <Filter categories={categories? categories: []}/>
             {isLoading ? (
-                <p>Loading...</p>
+                <Loader />
             ) : errorMessage ? (
                 <div className="flex justify-center items-center h-[200px]">
                    <FaExclamationTriangle className="text-slate-800 text-3xl mr-2" />
