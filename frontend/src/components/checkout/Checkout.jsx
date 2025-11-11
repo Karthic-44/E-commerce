0d89f2819ae3c2f821359aae9556bee41126235e
Karthic <karthics004@gmail.com>
2025-11-11 19:49:05 +0530

    Added Checkout

---
+++ frontend/src/components/checkout/Checkout.jsx
@@ -0,0 +1,41 @@
+import { Step, StepLabel, Stepper } from '@mui/material';
+import React, { useEffect, useState } from 'react'
+import AddressInfo from './AddressInfo';
+import { useDispatch } from 'react-redux';
+import { getUserAddresses } from '../../store/action';
+
+const Checkout = () => {
+
+  const [activeStep, setActiveStep] = useState(0);
+  const dispatch = useDispatch();
+
+  const steps = [
+    "Address",
+    "Payment Method",
+    "Order Summary",
+    "Payment",
+  ];
+
+useEffect(()=>{
+    dispatch(getUserAddresses());
+},[dispatch]);
+
+  return (
+    <div className='py-14 min-h-[calc(100vh-100px)]'>
+      <Stepper activeStep={activeStep} alternativeLabel>
+        {steps.map((label,index) => (
+          <Step key={index}>
+            <StepLabel>{label}</StepLabel>
+          </Step>
+        ))}
+
+      </Stepper>
+
+      <div className='mt-5'>
+        {activeStep === 0 && <AddressInfo />}
+      </div>
+    </div>
+  );
+}
+
+export default Checkout;
