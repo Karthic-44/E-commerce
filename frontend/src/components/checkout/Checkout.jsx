f751d0330101b16d252a8e85b645d533d8e0185e
Karthic <karthics004@gmail.com>
2025-11-24 15:11:03 +0530

    Added PaymentMethods

--- frontend/src/components/checkout/Checkout.jsx
+++ frontend/src/components/checkout/Checkout.jsx
@@ -7,17 +7,22 @@ import toast from 'react-hot-toast';
 import Skeleton from '../shared/Skeleton';
 import ErrorPage from '../shared/ErrorPage';
 import PaymentMethod from './PaymentMethod';
+import OrderSummary from './OrderSummary';
+import StripePayment from './StripePayment';
+import PaypalPayment from './PaypalPayment';
 
 const Checkout = () => {
 
   const [activeStep, setActiveStep] = useState(0);
   const dispatch = useDispatch();
   const {isLoading, errorMessage} = useSelector((state) => state.errors);
+  const {cart, totalPrice} = useSelector((state) => state.carts);
   const {address, selectedUserCheckoutAddress} = useSelector(
     (state) => state.auth 
   )
 
-  const paymentMethod = false;
+  const {paymentMethod} = useSelector((state) => state.payment);
+  
   const handleBack = () => {
         setActiveStep((prevStep) => prevStep - 1);
     };
@@ -68,6 +73,19 @@ useEffect(()=>{
         <div className='mt-5'>
         {activeStep === 0 && <AddressInfo address={address}/>}
         {activeStep === 1 && <PaymentMethod /> }
+        {activeStep === 2 && <OrderSummary 
+                                totalPrice={totalPrice}
+                                cart={cart}
+                                address={selectedUserCheckoutAddress}
+                                paymentMethod={paymentMethod}/> }
+         {activeStep === 3 && 
+            <>
+              {paymentMethod === "Stripe" ? (
+                <StripePayment />
+              ) : 
+                <PaypalPayment />
+              }
+            </> }
       </div>
       )}  
       
