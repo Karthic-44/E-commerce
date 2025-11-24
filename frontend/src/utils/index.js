17ee9e0109e566938803047af2985d4fa77ea023
Karthic <karthics004@gmail.com>
2025-11-24 19:03:13 +0530

    Added Admin/Seller navigation

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -1,4 +1,5 @@
 import { bannerImageOne,bannerImageThree, bannerImageTwo } from "./constant";
+import { FaBoxOpen, FaHome, FaShoppingCart, FaStore, FaThList } from "react-icons/fa";
 
 export const bannerLists = [
     {
@@ -24,3 +25,41 @@ export const bannerLists = [
 }
 ];
 
+export const adminNavigation = [
+  {
+    name: "Dashboard", 
+    href: "/admin", 
+    icon: FaHome, 
+    current: true 
+  }, {
+    name: "Orders", 
+    href: "/admin/orders", 
+    icon: FaShoppingCart
+  }, {
+    name: "Products", 
+    href: "/admin/products", 
+    icon: FaBoxOpen
+  }, {
+    name: "Categories", 
+    href: "/admin/categories", 
+    icon: FaThList
+  }, {
+    name: "Sellers", 
+    href: "/admin/sellers", 
+    icon: FaStore 
+  }
+];
+
+
+export const sellerNavigation = [
+  {
+    name: "Orders", 
+    href: "/admin/orders", 
+    icon: FaShoppingCart,
+    current: true 
+  }, {
+    name: "Products", 
+    href: "/admin/products", 
+    icon: FaBoxOpen
+  }
+];
