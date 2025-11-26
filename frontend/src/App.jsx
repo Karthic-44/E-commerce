566d6c20b18398de5f8a8c0a3f1501446d845d42
Karthic <karthics004@gmail.com>
2025-11-26 16:12:53 +0530

    Added Orders

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -13,6 +13,11 @@ import PrivateRoute from './components/PrivateRoute'
 import Register from './components/auth/Register'
 import Checkout from './components/checkout/Checkout'
 import AdminLayout from './components/admin/AdminLayout'
+import Dashboard from './components/admin/dashboard/Dashboard'
+import AdminProducts from './components/admin/products/AdminProducts'
+import Sellers from './components/admin/sellers/Sellers'
+import Category from './components/admin/categories/Category'
+import Orders from './components/admin/orders/Orders'
 
 function App() {
   return (
@@ -37,8 +42,14 @@ function App() {
           </Route>
 
           
-          <Route path='/' element={< PrivateRoute adminOnly/>}>
-            <Route path='/admin' element={<AdminLayout />} />
+          <Route path='/' element={<PrivateRoute adminOnly />}>
+            <Route path='/admin' element={ <AdminLayout />}>
+              <Route path='' element={<Dashboard />} />
+              <Route path='products' element={<AdminProducts />} />
+              <Route path='sellers' element={<Sellers />} />
+              <Route path='orders' element={<Orders />} />
+              <Route path='categories' element={<Category />} />
+            </Route>
             
           </Route>
 
