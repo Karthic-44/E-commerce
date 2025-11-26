1696c503147b1296fbbc923dd543e912375a9d4d
Karthic <karthics004@gmail.com>
2025-11-26 11:57:02 +0530

    Added AnalyticsServiceImpl

--- src/main/java/com/karthic/ecommerce/repositories/OrderRepository.java
+++ src/main/java/com/karthic/ecommerce/repositories/OrderRepository.java
@@ -2,8 +2,12 @@ package com.karthic.ecommerce.repositories;
 
 import com.karthic.ecommerce.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface OrderRepository extends JpaRepository<Order,Long> {
+
+    @Query("SELECT COALESCE(SUM(o.totalAmount), 0) FROM Order o")
+    Double getTotalRevenue();
 }
