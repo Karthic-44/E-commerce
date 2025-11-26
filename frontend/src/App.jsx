6c4a3b6870b2315ee4c9f89eedf4920d24ad9d83
Karthic <karthics004@gmail.com>
2025-11-26 11:01:49 +0530

    Added admin/seller path

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -37,7 +37,7 @@ function App() {
           </Route>
 
           
-          <Route path='/' element={< PrivateRoute />}>
+          <Route path='/' element={< PrivateRoute adminOnly/>}>
             <Route path='/admin' element={<AdminLayout />} />
             
           </Route>
